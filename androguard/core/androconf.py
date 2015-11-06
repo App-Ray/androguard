24379275baf0dc7418c86bcbb36c8477a0304e8c
dhilipsiva <dhilipsiva@gmail.com>
2015-11-06 15:20:35 +0530

    Fix some androguard errors

--- androguard/core/androconf.py
+++ androguard/core/androconf.py
@@ -21,7 +21,6 @@ import logging
 import types
 import random
 import string
-import imp
 
 ANDROGUARD_VERSION = "3.0-dev"
 
@@ -172,13 +171,13 @@ def long2int(l):
 def long2str(l):
     """Convert an integer to a string."""
     if type(l) not in (types.IntType, types.LongType):
-        raise ValueError, 'the input must be an integer'
+        raise ValueError('the input must be an integer')
 
     if l < 0:
-        raise ValueError, 'the input must be greater than 0'
+        raise ValueError('the input must be greater than 0')
     s = ''
     while l:
-        s = s + chr(l & 255L)
+        s = s + chr(l & 255)
         l >>= 8
 
     return s
@@ -187,9 +186,9 @@ def long2str(l):
 def str2long(s):
     """Convert a string to a long integer."""
     if type(s) not in (types.StringType, types.UnicodeType):
-        raise ValueError, 'the input must be a string'
+        raise ValueError('the input must be a string')
 
-    l = 0L
+    l = 0
     for i in s:
         l <<= 8
         l |= ord(i)
